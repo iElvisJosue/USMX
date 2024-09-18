baaca93ff4df5c5ca7f751eb3d31b40684a5ffcc
Elvis Josue <elvisjosue98@hotmail.com>
2024-09-17 19:11:44 -0600

    Cambio de URL

--- src/helpers/Urls.jsx
+++ src/helpers/Urls.jsx
@@ -1,3 +1,2 @@
-export const HOST = "http://localhost:5173/";
-// export const HOST_IMG = "http://localhost:4000";
-// export const HOST_PDF = "http://localhost:4000/PDF";
+// export const HOST = "http://localhost:5173/";
+export const HOST = "https://usmx.vercel.app/";
