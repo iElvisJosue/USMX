bccf470e338f0fd08c32c494ceb8203829a56125
Elvis Josue <elvisjosue98@hotmail.com>
2024-09-21 21:20:51 -0600

    Cambiando url del back

--- src/helpers/Urls.jsx
+++ src/helpers/Urls.jsx
@@ -1,4 +1,4 @@
 // export const HOST = "http://localhost:5173/";
 // export const HOST_PDF = "http://localhost:4000/PDF";
 export const HOST = "https://www.usmxxpress.net/";
-export const HOST_PDF = "www.usmxxpress.online:4000/PDF";
+export const HOST_PDF = "www.usmxxpress.online:4100/PDF";
