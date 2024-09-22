32c8c5850e99a06066fed9f30d1ae2b2ad8b346f
Elvis Josué <68576522+iElvisJosue@users.noreply.github.com>
2024-09-21 21:34:38 -0600

    Update Urls.jsx

--- src/helpers/Urls.jsx
+++ src/helpers/Urls.jsx
@@ -1,4 +1,4 @@
-export const HOST = "http://localhost:5173/";
-export const HOST_PDF = "http://localhost:4000/PDF";
-// export const HOST = "https://www.usmxxpress.net/";
-// export const HOST_PDF = "https://www.usmxxpress.online:4100/PDF";
+// export const HOST = "http://localhost:5173/";
+// export const HOST_PDF = "http://localhost:4000/PDF";
+export const HOST = "https://www.usmxxpress.net/";
+export const HOST_PDF = "https://www.usmxxpress.online:4100/PDF";
