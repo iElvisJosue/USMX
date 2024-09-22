19f63e71f77e1ca11d6eb5889e6b534245249320
Elvis Josue <elvisjosue98@hotmail.com>
2024-09-21 21:39:25 -0600

    Probando urls

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // UTILIZAR CUANDO ESTE EN PRODUCCIÓN
-  baseURL: "https://www.usmxxpress.online:4100/api",
+  baseURL: "https://www.usmxxpress.online:4000/api",
   // baseURL: "http://localhost:4000/api",
   withCredentials: true,
 });
